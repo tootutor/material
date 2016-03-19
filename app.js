0abe17ad26708ba5d8abb65fde826509ab524238
Chommaree Polyiam <Chommaree Polyiam>
2016-03-19 16:32:07 +0700

    update layout and theme

--- app.js
+++ app.js
@@ -18,12 +18,34 @@ ttoApp.config(function($mdThemingProvider) {
     'A400': '000000',
     'A700': '000000'
   });
+
+  $mdThemingProvider.definePalette('white', {
+    '50': 'ffffff',
+    '100': 'ffffff',
+    '200': 'ffffff',
+    '300': 'ffffff',
+    '400': 'ffffff',
+    '500': 'ffffff',
+    '600': 'ffffff',
+    '700': 'ffffff',
+    '800': 'ffffff',
+    '900': 'ffffff',
+    'A100': 'ffffff',
+    'A200': 'ffffff',
+    'A400': 'ffffff',
+    'A700': 'ffffff'
+  });
   
   $mdThemingProvider.theme('ttoDark')
     .primaryPalette('black')
     .accentPalette('blue')
-    .warnPalette('deep-orange')
+    .warnPalette('deep-orange');
     .dark();
+
+	$mdThemingProvider.theme('ttoOrange')
+    .primaryPalette('deep-orange')
+    .accentPalette('teal')
+    .warnPalette('orange');
 });
 
 ttoApp.controller('toolbarCtrl', ['$mdSidenav', function($mdSidenav) {
@@ -31,6 +53,17 @@ ttoApp.controller('toolbarCtrl', ['$mdSidenav', function($mdSidenav) {
   vm.toggleSidenav = function (menuId) {
     $mdSidenav(menuId).toggle();
   }
+}]);
+
+ttoApp.controller('pageCtrl', [function() {
+  var vm = this;
+  vm.cardList = [
+		{title: 'Title 001', name: 'Name 001'},
+		{title: 'Title 002', name: 'Name 002'},
+		{title: 'Title 003', name: 'Name 003'},
+		{title: 'Title 004', name: 'Name 004'},
+		{title: 'Title 005', name: 'Name 005'}
+	];
 }]);
 
 ttoApp.controller('leftNavCtrl', function() {
